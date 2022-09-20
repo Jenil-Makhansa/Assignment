c681e58386ab4a7fabdcef6bf8d77c99b10f353e
Jenil-Makhansa <109416024+Jenil-Makhansa@users.noreply.github.com>
2022-09-20 10:45:31 +0530

    Add files via upload

---
+++ Assignment-4/Practical_4_2.java
@@ -0,0 +1,38 @@
+/*
+    Name : Jenil Makhansa
+    ID   : 21CE065
+    Pr   : WAP to generate user defined exception using “throw” and “throws” keyword.
+*/
+
+import java.util.Scanner;
+
+class MyException extends Exception {
+
+    public MyException() {
+    }
+
+    public MyException(String message) {
+        super(message);
+    }
+
+}
+
+public class Practical_4_2 {
+    public static void main(String[] args) throws MyException {
+        try {
+            ReadValue();
+        } catch (MyException e) {
+            System.out.println(e);
+        }
+    }
+
+
+    static void ReadValue() throws MyException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int a = sc.nextInt();
+        sc.close();
+        if (a < 10)
+            throw new MyException("Value is less than 10.");
+    }
+}
